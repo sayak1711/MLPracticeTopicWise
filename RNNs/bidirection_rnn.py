3c0975a1ac8bd328ccd31c10a0607530defa04b4
Sayak Chattopadhyay <96sayak@gmail.com>
2021-03-16 12:42:30 +0530

    added few imports for running on GPU

--- RNNs/bidirection_rnn.py
+++ RNNs/bidirection_rnn.py
@@ -1,3 +1,8 @@
+# next 4 lines only required if running on GPU
+import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
 import pandas as pd
 import gensim.downloader as api
 from tensorflow.keras import Sequential
